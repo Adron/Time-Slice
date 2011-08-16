a6ada43b6e53a7b7c44e800d961e2bbc354dc629
Adron Hall <adronhall@gmail.com>
2011-08-16 15:27:02 -0700

    Added the hour and minute parsing in the constructure.

--- TimeSlicer.Tests/TimeSlicerTests.cs
+++ TimeSlicer.Tests/TimeSlicerTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 using Shouldly;
 
 namespace TimeSlicer.Tests
@@ -6,8 +7,9 @@ namespace TimeSlicer.Tests
     [TestFixture]
     public class with_time_slicer
     {
-        protected string sampleTimeOne = "2:30pm";
-        protected string sampleTimeTwo = "10:12am";
+        protected string sampleTimeOne = "10:12am";
+        protected string sampleTimeTwo = "2:30pm";
+
         protected string sampleBadTimeOne = "43:10am";
         protected string sampleBadTimeTwo = "2:120pm";
 
@@ -17,41 +19,43 @@ namespace TimeSlicer.Tests
             timePiece.ShouldNotBe(null);
         }
 
-        public void should_provide_error_message_for_invalid_time()
+        public void should_provide_the_hour_for_the_parsed_time()
         {
-            var timePiece = new TimePiece(sampleBadTimeOne);
-            timePiece.Error.ShouldMatch("Bad time entered. A valid time must be passed in for parsing.");
+            var timePiece = new TimePiece(sampleTimeOne);
+            timePiece.Hour.ShouldBe(10);
         }
 
-        public void should_return_appropriate_hour()
+        public void should_provide_the_hour_in_military_time_for_the_parsed_time()
         {
-            
+            var timePiece = new TimePiece(sampleTimeTwo);
+            timePiece.MilitaryHour.ShouldBe(14);
         }
 
-        public void should_return_appropriate_minute()
+        public void should_provide_the_minute()
         {
-
+            var timePiece = new TimePiece(sampleTimeOne);
+            timePiece.Minute.ShouldBe(12);
         }
 
-        public void should_return_appropriate_meridian()
+        public void should_provide_the_minute_for_military_parsing()
         {
-
+            var timePiece = new TimePiece(sampleTimeTwo);
+            timePiece.Minute.ShouldBe(30);
         }
-
-
     }
 
     public class TimePiece
     {
-        public TimePiece(string sampleTimeOne)
+        public TimePiece(string time)
         {
-
+            Hour = Convert.ToInt32(time.Split(Convert.ToChar(":"))[0]);
+            MilitaryHour = Convert.ToInt32(time.Split(Convert.ToChar(":"))[0]) + 12;
+            Minute = Convert.ToInt32(time.Split(Convert.ToChar(":"))[1]);
         }
 
-        public string Error
-        {
-            get { throw new System.NotImplementedException(); }
-            set { throw new System.NotImplementedException(); }
-        }
+        public string Error { get; set; }
+        public int Hour { get; set; }
+        public int MilitaryHour { get; set; }
+        public int Minute { get; set; }
     }
 }
